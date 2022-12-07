7246cbefd838e4d05e9a49c7c2748a77dbf76a9f
JeremyLi17 <HaotianLi0505@hotmail.com>
2022-12-07 03:29:40 -0500

    update format date function

--- Client/src/utils/FormatDate.java
+++ Client/src/utils/FormatDate.java
@@ -1,20 +1,20 @@
 package utils;
 
 import java.text.SimpleDateFormat;
-import java.util.Calendar;
 import java.util.Date;
+import java.util.TimeZone;
 
 /**
  * @author jeremy on 2022/12/7
  */
 public class FormatDate {
 
-    private static final String DATE_PATTERN = "MM/dd/yyyy";
+    private static final String DATE_PATTERN = "yyyy/MM/dd";
     private static final SimpleDateFormat DATE_FORMAT
             = new SimpleDateFormat(DATE_PATTERN);
 
     public static String getCurrentDate(Date date) {
-        Calendar calendar = Calendar.getInstance();
+//        DATE_FORMAT.setTimeZone(TimeZone.getTimeZone("GMT-5"));
         String curr_date = DATE_FORMAT.format(date);
 
         return curr_date;
